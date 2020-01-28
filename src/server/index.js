36d41d94dd1c10b53d02c9b3bcf6c4b57245c111
6ry0u <6ry0u@tutanota.com>
2020-01-28 23:01:15 +0100

    feat:test: Get all movies

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
-const routes = require('./routes/index').routes()
+const indexRoutes = require('./routes/index').routes()
+const moviesRoutes = require('./routes/movies').routes()
 
 const app = new Koa()
 const PORT = process.env.PORT || 1337
 
-app.use(routes)
+app.use(indexRoutes)
+app.use(moviesRoutes)
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
